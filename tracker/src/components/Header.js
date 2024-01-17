e6f0f82815cdb450dc5cf850832b535afa1a92db
Muma Brian <muma.neoworlder@gmail.com>
2024-01-17 02:19:04 +0100

    Finished Tracker task

--- tracker/src/components/Header.js
+++ tracker/src/components/Header.js
@@ -1,11 +1,30 @@
-import Button from "./Button"
-const Header = ({title}) => {
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
     return (
-        <header>
-            <h1>{title}</h1>
-            <Button text='Add' color='blue'></Button>       
+        <header className='header'>
+        <h1>{title}</h1>
+        {location.pathname === '/' && (
+            <Button
+            color={showAdd ? 'red' : 'green'}
+            text={showAdd ? 'Close' : 'Add'}
+            onClick={onAdd}
+            />
+        )}
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
